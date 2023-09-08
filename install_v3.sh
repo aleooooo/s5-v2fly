4f819059366c5f1158f6d94631b168dfa614b2f5
alln <16016128@qq.com>
2023-09-09 07:01:29 +0800

    Create install_v3.sh

---
+++ install_v3.sh
@@ -0,0 +1,29 @@
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+#BBR
+#wget –no-check-certificate https://raw.githubusercontent.com/aleooooo/s5-v2fly/main/install_bbr.sh 
+#chmod +x install_bbr.sh 
+#./install_bbr.sh
+
+#v2fly
+wget https://raw.githubusercontent.com/aleooooo/s5-v2fly/main/v2fly_install.sh
+chmod -R 777 v2fly_install.sh
+./v2fly_install.sh
+
+#wget -cO /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/aleooooo/s5-v2fly/main/v2fly_config.json
+chmod -R 777 /usr/local/etc/v2ray/config.json
+echo '{"inbounds":[{"port":443,"protocol":"vmess","settings":{"clients":[{"id":"ffffffff-ffff-ffff-ffff-ffffffffffff","alterId":0}]}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > /usr/local/etc/v2ray/config.json 
+
+#nodeJs
+yum -y install nodejs
+yum -y install npm
+npm update
+wget https://raw.githubusercontent.com/aleooooo/s5-v2fly/main/callback_v1.zip
+unzip callback_v1.zip -d /root/
+cd /root/
+npm install forever -g
+
+host=$1
+chmod +x /etc/rc.d/rc.local
+echo -e 'cd /root\nforever app.js\n' >> /etc/rc.d/rc.local
+reboot
